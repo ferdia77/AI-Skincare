d9765d3e5517034962c1a27fa82dd33a3a215bc2
David <kingqueensuited22@gmai.com>
2025-04-28 14:57:39 +0200

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="w-full flex items-center justify-end ">
+        <div>
+            <a className='px-3 text-xs '>
+                SKINSTRIC
+            </a>
+            <a className='text-xs'>
+                [INTRO]
+            </a>
+        </div>
+        <div>
+            <button className='bg-black text-white text-xs cursor-pointer'>
+                Enter Code
+            </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
