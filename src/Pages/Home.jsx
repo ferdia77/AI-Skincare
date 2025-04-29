8d085b9d30e2aefc1a083622bd8c2e2a4e9446c9
David <kingqueensuited22@gmai.com>
2025-04-29 16:35:30 +0200

    a

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <>
       <div className="w-full flex items-center justify-between ">
         <div>
-            <a className='px-3 text-xs '>
+            <a className='px-3 text-xs font-bold '>
                 SKINSTRIC
             </a>
             <a className='text-xs'>
@@ -13,12 +13,19 @@ const Home = () => {
             </a>
         </div>
         <div>
-            <button className='bg-black text-white text-xs cursor-pointer'>
+            <button className='bg-black text-white text-xs cursor-pointer mr-8 my-8 p-2 border
+            rounded-md'>
                 Enter Code
             </button>
         </div>
       </div>
-    </div>
+      <div className='w-full'>
+        <div className=' content-center text-7xl flex items-center justify-items-center'>
+            Sophisticated <br/> skincare
+        </div>
+      </div>
+      
+    </>
   )
 }
 
