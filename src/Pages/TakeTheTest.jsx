5c42965dbd33b10d3b6ca0bf2f91d6f56126eadd
David <kingqueensuited22@gmai.com>
2025-05-02 17:07:08 +0200

    a

--- src/Pages/TakeTheTest.jsx
+++ src/Pages/TakeTheTest.jsx
@@ -1,15 +1,24 @@
-import React from "react";
-
-const handleKeyDown = (e) => {
-    if (e.key === 'Enter') {
-        e.preventDefault();
-        input2Ref.current.focus();
-    }
-        
-}
-
+import React, { useState } from "react";
 
 const TakeTheTest = () => {
+    const[phase, setPhase] = useState(1)
+
+    https://us-central1-api-skinstric-ai.cloudfunctions.net/skinstricPhaseOne
+    
+    {
+        "name": "John Doe",
+        "location": ""
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            setPhase(2)
+            e.preventDefault();
+            input2Ref.current.focus();
+        }
+            
+    }
+
+
   return (
     <div>
       <div className="pl-12 pt-4">
@@ -19,10 +28,13 @@ const TakeTheTest = () => {
       <div className="pl-12 pt-6">To Start Analysis</div>
       <div className="relative flex flex-col items-center justify-center mb-40">
         <p className="text-[10px] text-gray-400 tracking-wider uppercase mb-1">CLICK TO TYPE</p>
-        <input className="text-3xl font-semibold text-center bg-transparent border-b
-         border-black focus:outline-none appearance-none w-[300px] leading-none pt-1"
-         placeholder="Introduce Yourself" onKeyDown={handleKeyDown} type="text" />
-         <input type="text" />
+        {phase === 1 ? <input className="text-3xl font-semibold text-center bg-transparent border-b
+            border-black focus:outline-none appearance-none w-[300px] leading-none pt-1"
+            placeholder="Introduce Yourself" onKeyDown={handleKeyDown} type="text" />  
+             : <input className="text-3xl font-semibold text-center bg-transparent border-b
+            border-black focus:outline-none appearance-none w-[300px] leading-none pt-1"
+            placeholder="What City are you from" type="text" />
+        }
       </div>
       <div className="absolute bottom-10 w-full flex justify-between px-10 cursor-pointer">
         <div className="relative w-12 h-12 left-4 flex items-center justify-center border 
