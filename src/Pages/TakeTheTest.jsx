7b7b66108152e96d624e4af8602f5362581dde31
David <kingqueensuited22@gmai.com>
2025-05-05 09:40:14 +0200

    a

--- src/Pages/TakeTheTest.jsx
+++ src/Pages/TakeTheTest.jsx
@@ -1,26 +1,48 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const TakeTheTest = () => {
-    const [phase, setPhase] = useState(1)
-    const [location, setLocation] = useState([])
-    const [name, setName] = useState([])
-    
-    
+  const [phase, setPhase] = useState(1);
+  const [location, setLocation] = useState([]);
+  const [name, setName] = useState([]);
+  const input2Ref = useRef(null);
 
-    const handleKeyDown = (e) => {
-        if (e.key === 'Enter') {
-            setPhase(2)
-            e.preventDefault();
-            input2Ref.current.focus();
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      setPhase(2);
+      e.preventDefault();
+
+      if (phase === 1) {
+        setPhase(2);
+        //waiting for next render cycle before focusing setTimeout
+        setTimeout(() => {
+          input2Ref.current.focus();
+        }, 0);
+      } else {
+        //phase2 complete: send data to backend
+        passValueBackend();
+      }
+    }
+  };
+
+  async function passValueBackend() {
+    try {
+      const response = await fetch(
+        "https://us-central1-api-skinstric-ai.cloudfunctions.net/skinstricPhaseOne",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json,",
+          },
+          body: JSON.stringify({ name, location }),
         }
-            
-    }
+      );
 
-    async function passValueBackend() {
-        const data = fetch("https://us-central1-api-skinstric-ai.cloudfunctions.net/skinstricPhaseOne")
-        console.log(data)
+      const result = await response.json();
+      console.log("Backend response:", result);
+    } catch (error) {
+      console.error("Error sendng data to backend:", error);
     }
-
+  }
 
   return (
     <div>
@@ -30,19 +52,41 @@ const TakeTheTest = () => {
       </div>
       <div className="pl-12 pt-6">To Start Analysis</div>
       <div className="relative flex flex-col items-center justify-center mb-40">
-        <p className="text-[10px] text-gray-400 tracking-wider uppercase mb-1">CLICK TO TYPE</p>
-        {phase === 1 ? <input className="text-3xl font-semibold text-center bg-transparent border-b
+        <p className="text-[10px] text-gray-400 tracking-wider uppercase mb-1">
+          CLICK TO TYPE
+        </p>
+        {phase === 1 ? (
+          <input
+            ref={input2Ref}
+            className="text-3xl font-semibold text-center bg-transparent border-b
             border-black focus:outline-none appearance-none w-[300px] leading-none pt-1"
-            placeholder="Introduce Yourself" onKeyDown={handleKeyDown} type="text" value={name} onChange={(event) => setName(event.target.value)} />  
-             : <input className="text-3xl font-semibold text-center bg-transparent border-b
-            border-black focus:outline-none appearance-none w-[300px] leading-none pt-1"
-            placeholder="What City are you from" value={location} onChange={(event) => setLocation(event.target.value)} type="text" />
-        }
+            placeholder="Introduce Yourself"
+            onKeyDown={handleKeyDown}
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        ) : (
+          <input
+            ref={input2Ref}
+            className="text-3xl font-semibold text-center bg-transparent border-b
+            border-black focus:outline-none appearance-none w-[320px] leading-none pt-1"
+            placeholder="What City are you from"
+            onKeyDown={handleKeyDown}
+            value={location}
+            onChange={(event) => setLocation(event.target.value)}
+            type="text"
+          />
+        )}
       </div>
       <div className="absolute bottom-10 w-full flex justify-between px-10 cursor-pointer">
-        <div className="relative w-12 h-12 left-4 flex items-center justify-center border 
-        border-black rotate-45 scale-[0.85]">
-            <span className="absolute rotate-[-45deg] text-xs font-semibold">BACK</span>
+        <div
+          className="relative w-12 h-12 left-4 flex items-center justify-center border 
+        border-black rotate-45 scale-[0.85]"
+        >
+          <span className="absolute rotate-[-45deg] text-xs font-semibold">
+            BACK
+          </span>
         </div>
         <a className="absolute inset-0" label="BACK" href="/"></a>
       </div>
